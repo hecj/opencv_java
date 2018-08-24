3a6c2b96758e4f5f214f1205a3c2516aa16f75f4
hechaojie <25693780@qq.com>
2018-08-24 18:52:10 +0800

    up

---
+++ src/test/java/test/opencv/Grayscaler.java
@@ -0,0 +1,28 @@
+package test.opencv;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+/**
+ * 图片灰度化处理
+ * @author hecj
+ */
+public class Grayscaler {
+
+	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		// 第一种 加载时灰度
+		Mat mat = Imgcodecs.imread("/data/data/WindowsLogo.jpg", Imgcodecs.IMREAD_GRAYSCALE);
+		Imgcodecs.imwrite("/Users/hecj/Desktop/WindowsLogo.jpg", mat);
+		
+		// 第二种 转换灰度图像
+		Mat src = Imgcodecs.imread("/data/data/WindowsLogo.jpg", Imgcodecs.IMREAD_UNCHANGED);
+		Mat dst = new Mat();
+		Imgproc.cvtColor(src, dst, Imgcodecs.IMREAD_GRAYSCALE);
+		Imgcodecs.imwrite("/Users/hecj/Desktop/WindowsLogo2.jpg", mat);
+
+	}
+
+}
