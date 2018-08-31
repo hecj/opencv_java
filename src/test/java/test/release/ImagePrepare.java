ec03176e7933b6abc9b3706355a0eb3eb1fad37e
hechaojie <25693780@qq.com>
2018-08-31 11:59:47 +0800

    up

--- src/test/java/test/release/ImagePrepare.java
+++ src/test/java/test/release/ImagePrepare.java
@@ -10,17 +10,20 @@ import org.opencv.imgproc.Imgproc;
 
 /**
  * 图片批量预处理 
- * @author hecj
+ * @author hecj 
  */
 public class ImagePrepare {
 
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		// 目录
-		String path = "/Users/hecj/Desktop/ocrdata/20180830";
+		String path = "/Users/hecj/Desktop/ocrdata/20180830/image2";
 		File dir = new File(path);
 		String files [] = dir.list();
 		for(String file : files) {
+			if(".DS_Store".equals(file)) {
+				continue;
+			}
 			dealImage(path, file);
 		}
 	}
@@ -38,7 +41,7 @@ public class ImagePrepare {
 		// 二值
 		Mat erzhi = new Mat();
 		Imgproc.threshold(hui, erzhi, 0, 255, Imgproc.THRESH_BINARY | Imgproc.THRESH_OTSU);// 灰度图像二值化
-		// 去噪
+//		// 去噪
 		Mat quzao = new Mat();
 		Imgproc.medianBlur(erzhi, quzao, 7);
 		Imgcodecs.imwrite(dir+"/ocr_"+file, quzao);
@@ -46,7 +49,7 @@ public class ImagePrepare {
 		src.release();
 		hui.release();
 		erzhi.release();
-		quzao.release();
+//		quzao.release();
 		
 	}
 	
