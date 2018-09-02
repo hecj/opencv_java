7fd187b3351e4285382d3f2c7f106dfc2be8415f
何超杰 <hechaojie@xylink.cn>
2018-09-03 00:56:21 +0800

    1

--- src/test/java/test/opencv/ImageZaoSheng.java
+++ src/test/java/test/opencv/ImageZaoSheng.java
@@ -49,10 +49,83 @@ public class ImageZaoSheng {
 	@Test
 	public void medianBlur() {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		Mat src = Imgcodecs.imread("/Users/hecj/Desktop/car/erzhi.jpeg");
-		Mat dst = src.clone();
-	    Imgproc.medianBlur(src, dst, 7);
-		Imgcodecs.imwrite("/Users/hecj/Desktop/car/erzhi_3.jpeg", dst);
+		Mat src = Imgcodecs.imread("/Users/hecj/Desktop/p3.jpeg");
+		Mat dst = new Mat();
+		// 灰色
+		Imgproc.cvtColor(src, dst, Imgproc.COLOR_BGR2GRAY);
+		Imgcodecs.imwrite("/Users/hecj/Desktop/p31.jpeg", dst);
+		// 二值滤波
+		Mat threshold = new Mat();
+        Imgproc.threshold(dst, threshold, 0, 255, Imgproc.THRESH_BINARY | Imgproc.THRESH_OTSU);//灰度图像二值化
+        Imgcodecs.imwrite("/Users/hecj/Desktop/p311.jpeg", threshold);
+        // 中值滤波
+		Mat median = new Mat();
+	    Imgproc.medianBlur(threshold, median, 5);
+	    Imgcodecs.imwrite("/Users/hecj/Desktop/p3111.jpeg", median);
+	}
+	
+	/**
+	 * 最大值滤波(膨胀)
+	 */
+	@Test
+	public void dilate() {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		Mat src = Imgcodecs.imread("/Users/hecj/Desktop/p3.jpeg");
+		Mat dst = new Mat();
+		// 灰色
+		Imgproc.cvtColor(src, dst, Imgproc.COLOR_BGR2GRAY);
+		Imgcodecs.imwrite("/Users/hecj/Desktop/p31.jpeg", dst);
+		// 二值滤波
+		Mat threshold = new Mat();
+		Imgproc.threshold(dst, threshold, 0, 255, Imgproc.THRESH_BINARY | Imgproc.THRESH_OTSU);//灰度图像二值化
+		Imgcodecs.imwrite("/Users/hecj/Desktop/p311.jpeg", threshold);
+       
+		// 卷积核
+        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3,3));
+        // 最大值滤波
+		Mat dilate = new Mat();
+	    Imgproc.dilate(threshold, dilate, kernel);
+	    Imgcodecs.imwrite("/Users/hecj/Desktop/p3111.jpeg", dilate);
+	}
+	
+	/**
+	 * 中值滤波->膨胀
+	 */
+	@Test
+	public void medianBlurdilate() {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		Mat src = Imgcodecs.imread("/Users/hecj/Desktop/p311111.jpeg");
+		// 卷积核
+        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3,3));
+        // 最大值滤波
+		Mat dilate = new Mat();
+	    Imgproc.dilate(src, dilate, kernel);
+	    Imgcodecs.imwrite("/Users/hecj/Desktop/p3111111.jpeg", dilate);
+	}
+	
+	/**
+	 * 最小值滤波(腐蚀)
+	 */
+	@Test
+	public void erode() {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		Mat src = Imgcodecs.imread("/Users/hecj/Desktop/p3.jpeg");
+		Mat dst = new Mat();
+		// 灰色
+		Imgproc.cvtColor(src, dst, Imgproc.COLOR_BGR2GRAY);
+		Imgcodecs.imwrite("/Users/hecj/Desktop/p31.jpeg", dst);
+		// 二值化
+		Mat threshold = new Mat();
+		Imgproc.threshold(dst, threshold, 0, 255, Imgproc.THRESH_BINARY | Imgproc.THRESH_OTSU);//灰度图像二值化
+		Imgcodecs.imwrite("/Users/hecj/Desktop/p311.jpeg", threshold);
+        // 卷积核
+        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3,3));
+        // 最小值滤波
+		Mat dilate = new Mat();
+	    Imgproc.erode(threshold, dilate, kernel);
+	    Imgcodecs.imwrite("/Users/hecj/Desktop/p3111.jpeg", dilate);
 	}
 
+	;
+	
 }
